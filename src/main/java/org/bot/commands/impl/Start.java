8d6b528b38fb899ea82eb3460011d093df26ae0b
anizhegorodtsev <freesafesoft@gmail.com>
2018-06-12 10:15:28 +0200

    Start command resources fix

--- src/main/java/org/bot/commands/impl/Start.java
+++ src/main/java/org/bot/commands/impl/Start.java
@@ -1,6 +1,7 @@
 package org.bot.commands.impl;
 
-import org.bot.AppData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.bot.commands.BotCommand;
 import org.bot.commands.Command;
 import org.bot.commands.CommandResultHandler;
@@ -9,6 +10,8 @@ import org.telegram.telegrambots.api.objects.Update;
 
 @BotCommand(name = "start")
 public class Start extends Command {
+    private static final Logger log = LogManager.getLogger(Start.class);
+
     @Override
     public void process(CommandResultHandler handler, Update update) {
         String startMessage = getStartMessage(update.getMessage().getChat().getFirstName());
@@ -17,7 +20,9 @@ public class Start extends Command {
 
     private String getStartMessage(String userName) {
         String helloMessage = getResource("command.start.helloMessage");
+        log.info("helloMessage: " + helloMessage);
         userName = userName == null ? getResource("command.start.helloMessage.defaultUser") : userName;
+        log.info("userName: " + userName);
         return String.format(helloMessage, userName);
     }
 }
