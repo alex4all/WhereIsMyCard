8e1958befb14305dfdbbe5a2c0c32f01b16f1074
anizhegorodtsev <freesafesoft@gmail.com>
2018-06-13 11:09:28 +0200

    help command translated

--- src/main/java/org/bot/commands/impl/Help.java
+++ src/main/java/org/bot/commands/impl/Help.java
@@ -3,10 +3,16 @@ package org.bot.commands.impl;
 import org.bot.commands.BotCommand;
 import org.bot.commands.Command;
 import org.bot.commands.CommandResultHandler;
+import org.bot.utils.EditText;
 import org.telegram.telegrambots.api.objects.Update;
 
 @BotCommand(name = "help")
 public class Help extends Command {
+    private static final String FIRST_APPOINTMENT = EditText.bold("/first_appointment");
+    private static final String DATE_INFO = EditText.bold("/date_info");
+    private static final String LINK = EditText.bold("/link");
+    private static final String GITHUB = EditText.bold("/github");
+    private static final String HELP = EditText.bold("/help");
 
     public Help(CommandResultHandler handler, Update update) {
         super(handler, update);
@@ -14,17 +20,13 @@ public class Help extends Command {
 
     @Override
     public void process(Update update) {
-        sendMessage(generateHelpMessage());
+        String help = new StringBuilder()
+                .append(FIRST_APPOINTMENT).append(" - ").append(getResource("command.help.firstAppointment")).append(System.lineSeparator())
+                .append(DATE_INFO).append(" - ").append(getResource("command.help.dateInfo")).append(System.lineSeparator())
+                .append(LINK).append(" - ").append(getResource("command.help.link")).append(System.lineSeparator())
+                .append(GITHUB).append(" - ").append(getResource("command.help.github")).append(System.lineSeparator())
+                .append(HELP).append(" - ").append(getResource("command.help.help")).append(System.lineSeparator())
+                .toString();
+        sendMessage(help);
     }
-
-    private String generateHelpMessage() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("<b>/first_appointment</b> - get first available appointment date").append(System.lineSeparator());
-        builder.append("<b>/date_info</b> - get information about selected date").append(System.lineSeparator());
-        builder.append("<b>/link</b> - get Urzad URL").append(System.lineSeparator());
-        builder.append("<b>/github</b> - source code").append(System.lineSeparator());
-        builder.append("<b>/help</b> - list of available commands");
-        return builder.toString();
-    }
-
 }
